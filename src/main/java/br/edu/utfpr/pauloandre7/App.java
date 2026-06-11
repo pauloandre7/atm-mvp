d9a867c6f5ac4086d65362d378d47fac4de23bec
pauloandre7 <andrepaulos.2001@gmail.com>
2026-06-11 13:57:50 -0300

    git init: Initialize Maven project structure

---
+++ src/main/java/br/edu/utfpr/pauloandre7/App.java
@@ -0,0 +1,10 @@
+package br.edu.utfpr.pauloandre7;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
