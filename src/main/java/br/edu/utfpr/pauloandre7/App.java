7073419e9cf5310810fca6686392d4447584aba4
pauloandre7 <andrepaulos.2001@gmail.com>
2026-06-11 18:34:37 -0300

    feat: implements ATM interface initialization in App class

--- src/main/java/br/edu/utfpr/pauloandre7/App.java
+++ src/main/java/br/edu/utfpr/pauloandre7/App.java
@@ -1,10 +1,27 @@
 package br.edu.utfpr.pauloandre7;
 
-/**
- * Hello world!
- */
+import javax.swing.SwingUtilities;
+
+import br.edu.utfpr.pauloandre7.presenters.AtmPresenter;
+import br.edu.utfpr.pauloandre7.repositories.ContaRepository;
+import br.edu.utfpr.pauloandre7.repositories.IContaRepository;
+import br.edu.utfpr.pauloandre7.repositories.ITransacaoRepository;
+import br.edu.utfpr.pauloandre7.repositories.TransacaoRepository;
+import br.edu.utfpr.pauloandre7.seeds.ContaSeeder;
+import br.edu.utfpr.pauloandre7.views.swing.AtmSwingView;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SwingUtilities.invokeLater(() -> {
+            IContaRepository contaRepository = new ContaRepository();
+            ITransacaoRepository transacaoRepository = new TransacaoRepository();
+
+            new ContaSeeder().seed(contaRepository);
+
+            AtmSwingView view = new AtmSwingView(contaRepository);
+            AtmPresenter presenter = new AtmPresenter(view, contaRepository, transacaoRepository);
+            view.setPresenter(presenter);
+            view.mostrar();
+        });
     }
 }
